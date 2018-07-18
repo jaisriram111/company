8793c06fae6fd4180e0b2a3ab750386cbec6ebb6
siva <sivaeleswaram@yahoo.com>
2018-07-18 09:20:14 -0700

    modify ssh

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ if [ $ENVIRONMENT = "QA" ]; then
       sshpass -p "1234" scp target/companybank.war priya@$i:/home/priya/soft/apache-tomcat-8.5.32/webapps
 
       echo "starting tomcat server $i"
-      shpass -p "1234" ssh priya@$i "JAVA_HOME=/home/priya/soft/jdk1.8.0_171" "/home/priya/soft/apache-tomcat-8.5.32/bin/./startup.sh"
+      sshpass -p "1234" ssh priya@$i "JAVA_HOME=/home/priya/soft/jdk1.8.0_171" "/home/priya/soft/apache-tomcat-8.5.32/bin/./startup.sh"
     done
   echo "deploy success"
 fi
