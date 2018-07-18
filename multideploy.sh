ccf162367af8a84df2ff68100dbfc092ed4f008f
siva <sivaeleswaram@yahoo.com>
2018-07-18 05:00:24 -0700

    ip and deployee script file

---
+++ multideploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for i in `cat ip.txt`
+ do
+   echo "Deploying war file in to $i QA server"
+    sleep 3
+     sshpass -p "1234" scp target/companybank.war priya@$i:/home/priya/soft/apache-tomcat-8.5.32/webapps
+#echo ip:$i
+   echo "Starting tomcat server  in $i QA server"
+     sshpass -p "1234" ssh priya@$i "JAVA_HOME=/home/priya/soft/jdk1.8.0_171" "/home/priya/soft/apache-tomcat-8.5.32/bin/./startup.sh"
+ done
+echo "Deployement success"
